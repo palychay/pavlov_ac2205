0642231b0424927fc59c15dbc77546e644126f04
palychay <118565996+palychay@users.noreply.github.com>
2023-09-08 19:04:22 +0300

    add struct pipe and kc

--- pavlov_ac.cpp
+++ pavlov_ac.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+struct Pipe  // труба
+{
+    string kilometr_name;
+    double length;
+    int diametr;
+    bool remont;
+};
+
+struct KC // компрессорная станция
+{
+    string name;
+    int kolich_ceh;
+    int kolich_ceh_v_rabote;
+    double effectivnost;
+};
+
+
+
 int main(){
     return 0;
 }
