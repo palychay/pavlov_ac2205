bef83d402a1129c8a6775d008626b37931ddde58
palychay <118565996+palychay@users.noreply.github.com>
2023-09-11 18:18:34 +0300

    add menu

--- pavlov_ac.cpp
+++ pavlov_ac.cpp
@@ -66,11 +66,60 @@ void Print_KC(KC kc){   //output new kc
 }
 
 
+
+
+void Menu(){
+    bool k = 1;
+    while (k){
+        cout << "Menu\n";
+        cout << " 1. Добавить трубу\n";
+        cout << " 2. Добавить КС\n";
+        cout << " 3. Просмотр всех объектов\n";
+        cout << " 4. Редактировать трубу\n";
+        cout << " 5. Редактировать КС\n";
+        cout << " 6. Сохранить\n";
+        cout << " 7. Загрузить\n";
+        cout << " 0. Выход\n";
+        int n;
+        cout << "vvod number: ";
+        cin >> n;
+
+
+        switch (n)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 0:
+            cout << "ffff";
+            k = 0;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+
+
+
 int main(){
-    Pipe p = New_Pipe();
+   /* Pipe p = New_Pipe();
     Print_Pipe(p);
 
     KC kc = New_KC();
-    Print_KC(kc);
+    Print_KC(kc);*/
+    Menu();
     return 0;
 }
